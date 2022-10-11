11e81d08174b439b3ae1e40a230e7f385215a244
mysticalSamurai <mysticalSamurai@protonmail.com>
2022-10-11 01:14:45 -0500

    First build with simple GUI

---
+++ src/main/java/com/mysticalSamurai/GamePanel.java
@@ -0,0 +1,195 @@
+package com.mysticalSamurai;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import java.util.Random;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.RenderingHints;
+import java.awt.geom.*;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25; // How big we want the apples to be
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
+    static final int DELAY = 50; // Game speed
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6; // Initial body parts of snake
+    int applesEaten;
+    int appleX; // x coordinate of apple [random]
+    int appleY;
+    char direction = 'R'; // R L U D
+    boolean running = false;
+    boolean directionChanged = false;
+    Timer timer;
+    Random random;
+    BufferedImage image;
+
+    GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+    }
+
+    public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        // try {
+        //     image = ImageIO.read(new File("/home/user/Downloads/drawing.png"));
+        // } catch (IOException ex) {
+        //     System.out.println("Could not read image");
+        // }
+        // g2.drawImage(image, 0, 0, this);
+        if (running) {
+            g2.setPaint(Color.red);
+            g2.fill(new Ellipse2D.Double(appleX, appleY, UNIT_SIZE, UNIT_SIZE));
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    g2.setPaint(Color.green);
+                    g2.fill(new Rectangle2D.Double(x[i], y[i], UNIT_SIZE, UNIT_SIZE));
+                } else {
+                    g2.setPaint(new Color(45, 180, 0));
+                    g2.fill(new Rectangle2D.Double(x[i], y[i], UNIT_SIZE, UNIT_SIZE));
+                }
+            }
+        } else {
+            gameOver(g2);
+        }
+    }
+
+    public void newApple() {
+        appleX = random.nextInt(SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE;
+        appleY = random.nextInt(SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+    }
+
+    public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
+    }
+
+    public void checkApple() {
+        if (x[0] == appleX && y[0] == appleY) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
+    }
+
+    public void checkCollisions() {
+        for (int i = bodyParts; i > 0; i--) {
+            if (x[0] == x[i] && y[0] == y[i] && !directionChanged) {
+                running = false;
+                directionChanged = false;
+            }
+        }
+        if (x[0] < 0 || x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        if (y[0] < 0 || y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+        if (!running) {
+            timer.stop();
+        }
+    }
+
+    public void gameOver(Graphics2D g2) {
+        g2.setPaint(Color.red);
+        g2.setFont(new Font("Rubik", Font.BOLD, 40));
+        FontMetrics fontMetrics = getFontMetrics(g2.getFont());
+        g2.drawString("Score: " + applesEaten, (SCREEN_WIDTH - fontMetrics.stringWidth("Score: " + applesEaten)) / 2, g2.getFont().getSize());
+        g2.setFont(new Font("Rubik", Font.BOLD, 75));
+        fontMetrics = getFontMetrics(g2.getFont());
+        g2.drawString("Game Over", (SCREEN_WIDTH - fontMetrics.stringWidth("Game Over")) / 2, SCREEN_HEIGHT / 2);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        // Object obj = e.getSource();
+        // System.out.println(obj.getClass());
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_UP:
+                    if (direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+            }
+            directionChanged = true;
+        }
+    }
+
+}
